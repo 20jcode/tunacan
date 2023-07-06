cb2bc077286335837cd68c47f7369887b5fb8989
mac <20j.code@gmail.com>
2023-07-06 09:53:45 +0900

    Initial commit

---
+++ src/main/java/org/tunacan/body/TunaCan.java
@@ -0,0 +1,10 @@
+package org.tunacan.body;
+
+public class TunaCan {
+
+	public String tunaCanName;
+
+	public int tunaCanValue;
+
+	public String tunaCanInfo;
+}
---
+++ src/main/java/org/tunacan/open/openable.java
@@ -0,0 +1,6 @@
+package org.tunacan.open;
+
+public interface openable {
+
+	void open();
+}
